3f4e8024a7a4f4b90de3edf3404c94c0143c77bf
stjepanz <51213719+stjepanz@users.noreply.github.com>
2021-02-25 12:21:34 +0100

    Automatsko punjenje, provjeravanje i updateanje baze sa podacima iz HNB-a Endpoint za popunjavanje i provjeru praznina u bazi Ispravljena greska u updateanju Security - BCrypt + JWT

--- src/main/java/com/hnb/security/config/AppSecurityConfig.java
+++ src/main/java/com/hnb/security/config/AppSecurityConfig.java
@@ -42,6 +42,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        
 
         http.authorizeRequests()
                 .antMatchers("/authenticate").permitAll()
