0158f8d7f9c02d020038bc215122528e8a5ec671
stjepanz <51213719+stjepanz@users.noreply.github.com>
2021-03-08 13:37:17 +0100

    Automatsko punjenje, provjeravanje i updateanje baze sa podacima iz HNB-a Endpoint za popunjavanje i provjeru praznina u bazi Ispravljena greska u updateanju Security - BCrypt + JWT Download excel fila za neki period i valutu Ubaceni custom errori Logiranje korisnikovih akcija u bazi (upgradeano) Optional endpoint - query

--- src/main/java/com/hnb/app/controller/LoggerController.java
+++ src/main/java/com/hnb/app/controller/LoggerController.java
@@ -3,9 +3,11 @@ package com.hnb.app.controller;
 import com.hnb.app.service.LoggerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,9 +18,11 @@ public class LoggerController {
     LoggerService loggerService;
 
     @GetMapping("/log")
-    public List<String> getLogoviUserDatumi(@RequestParam (required = false) String user,
-                              @RequestParam (required = false) String datumOd,
-                              @RequestParam (required = false) String datumDo){
-        return loggerService.getLogovi(user, datumOd, datumDo);
+    public List<String> getLogoviUserDatumi(@CurrentSecurityContext(expression="authentication?.name") String loggedUser,
+                                            @RequestParam (required = false) String user,
+                                            @RequestParam (required = false) String datumOd,
+                                            @RequestParam (required = false) String datumDo,
+                                            HttpServletResponse response){
+        return loggerService.getLogovi(user, datumOd, datumDo, loggedUser, response);
     }
 }
--- src/main/java/com/hnb/app/service/LoggerService.java
+++ src/main/java/com/hnb/app/service/LoggerService.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -16,6 +17,10 @@ import java.util.List;
 
 @Service
 public class LoggerService {
+
+    @Autowired
+    LoggerService loggerService;
+
 
     @Autowired
     Queries queries;
@@ -31,44 +36,87 @@ public class LoggerService {
         repository.save(logovi);
     }
 
-    public List<String> getLogovi(String user,String datumOd,String datumDo) {
+    public List<String> getLogovi(String user,
+                                  String datumOd,
+                                  String datumDo,
+                                  String loggedUser,
+                                  HttpServletResponse response) {
         LocalDate start;
         LocalDate end;
         if (datumOd == null && datumDo == null) {
             if (user == null) {
+                loggerService.spremiLog("Dohvacanje logova za danasnji datum", "/log", loggedUser, response.getStatus());
                 return queries.getLogoviPoDatumu(LocalDate.now(), LocalDate.now().plusDays(1));
             } else {
+                loggerService.spremiLog("Dohvacanje logova za danasnji datum za usera: " + user, "/log", loggedUser, response.getStatus());
                 return queries.getLogoviPoDatumuIUseru(LocalDate.now(), LocalDate.now().plusDays(1), user);
             }
-        }
-        else if (datumOd != null && datumDo != null) {
+        } else if (datumOd != null && datumDo != null) {
             try {
                 start = LocalDate.parse(datumOd);
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
+                response.setStatus(400);
+                loggerService.spremiLog("Error - Dohvacanje logova - Datum nije ispravan", "/log", loggedUser, response.getStatus());
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Datum pocetka koji ste unijeli nije ispravan", e);
             }
-            try{
+            try {
                 end = LocalDate.parse(datumDo);
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
+                response.setStatus(400);
+                loggerService.spremiLog("Error - Dohvacanje logova - Datum nije ispravan", "/log", loggedUser, response.getStatus());
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Datum kraja koji ste unijeli nije ispravan", e);
             }
-            if(start.isBefore((LocalDate.parse(queries.getPrviDatumLogger().toString().substring(0, 10))))) {
+            if (start.isBefore((LocalDate.parse(queries.getPrviDatumLogger().toString().substring(0, 10))))) {
+                response.setStatus(400);
+                loggerService.spremiLog("Error - Dohvacanje logova - Uneseni datum je prije datuma prvog loga u bazi", "/log", loggedUser, response.getStatus());
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Datum prvog loga je: " + queries.getPrviDatumLogger().toString().substring(0, 10));
             }
-            if(end.isAfter((LocalDate.parse(queries.getZadnjiDatumLogger().toString().substring(0, 10))))) {
+            if (end.isAfter((LocalDate.parse(queries.getZadnjiDatumLogger().toString().substring(0, 10))))) {
+                response.setStatus(400);
+                loggerService.spremiLog("Error - Dohvacanje logova - Uneseni datum jos nije dosao", "/log", loggedUser, response.getStatus());
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Datum zadnjeg loga je: " + queries.getZadnjiDatumLogger().toString().substring(0, 10));
             }
-            if(user == null) {
+            if (user == null) {
+                loggerService.spremiLog("Dohvacanje logova za datume od: " + datumOd + " do: " + datumDo, "/log", loggedUser, response.getStatus());
                 return queries.getLogoviPoDatumu(start, end.plusDays(1));
-            }
-            else{
+            } else {
+                loggerService.spremiLog("Dohvacanje logova za datume od: " + datumOd + " do: " + datumDo + " za usera: " + user, "/log", loggedUser, response.getStatus());
                 return queries.getLogoviPoDatumuIUseru(start, end.plusDays(1), user);
             }
         }
-        else{
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Trebaju biti unesena oba datuma ili ne treba unijeti ni jedan datum");
+        else {
+            if (datumDo == null) {
+                try {
+                    start = LocalDate.parse(datumOd);
+                } catch (Exception e) {
+                    response.setStatus(400);
+                    loggerService.spremiLog("Error - Dohvacanje logova - Datum nije ispravan", "/log", loggedUser, response.getStatus());
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Datum koji ste unijeli nije ispravan (format: GGGG-MM-DD)", e);
+                }
+                if (user == null) {
+                    loggerService.spremiLog("Dohvacanje logova za datum: " + start, "/log", loggedUser, response.getStatus());
+                    return queries.getLogoviPoDatumu(start, start.plusDays(1));
+                } else {
+                    loggerService.spremiLog("Dohvacanje logova za datum: " + start +" za usera: "+user, "/log", loggedUser, response.getStatus());
+                    return queries.getLogoviPoDatumuIUseru(start, start.plusDays(1), user);
+                }
+            }
+            else {
+                try {
+                    start = LocalDate.parse(datumDo);
+                } catch (Exception e) {
+                    response.setStatus(400);
+                    loggerService.spremiLog("Error - Dohvacanje logova - Datum nije ispravan", "/log", loggedUser, response.getStatus());
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Datum koji ste unijeli nije ispravan (format: GGGG-MM-DD)", e);
+                }
+                if (user == null) {
+                    loggerService.spremiLog("Dohvacanje logova za datum: " + start, "/log", loggedUser, response.getStatus());
+                    return queries.getLogoviPoDatumu(start, start.plusDays(1));
+                } else {
+                    loggerService.spremiLog("Dohvacanje logova za datum: " + start +" za usera: "+user, "/log", loggedUser, response.getStatus());
+                    return queries.getLogoviPoDatumuIUseru(start, start.plusDays(1), user);
+                }
+            }
         }
     }
 }
