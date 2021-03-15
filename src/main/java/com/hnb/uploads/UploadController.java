a1b4bd0d15fb717285e40ead56f9dc93e0145f74
stjepanz <51213719+stjepanz@users.noreply.github.com>
2021-03-15 10:18:32 +0100

    Popravljanje zadnje 3 verzije. CRUD preko username-a, a ne id-a UsersGet DTO klasa Sreden download, logger, security Sreden upload, logger, security

--- src/main/java/com/hnb/uploads/UploadController.java
+++ src/main/java/com/hnb/uploads/UploadController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class UploadController {
 
@@ -15,7 +18,9 @@ public class UploadController {
     }
 
     @PostMapping("upload")
-    public void upload(@RequestParam("file")MultipartFile file) throws Exception {
-        uploadService.upload(file);
+    public void upload(@RequestParam("file")MultipartFile file,
+                       HttpServletResponse response,
+                       HttpServletRequest request) throws Exception {
+        uploadService.upload(file, request.getUserPrincipal().getName(), response);
     }
 }
--- src/main/java/com/hnb/uploads/UploadService.java
+++ src/main/java/com/hnb/uploads/UploadService.java
@@ -1,26 +1,33 @@
 package com.hnb.uploads;
 
 import com.hnb.app.models.TecajeviUpload;
+import com.hnb.app.service.LoggerService;
 import org.apache.poi.ss.usermodel.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.text.DateFormat;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 @Service
 public class UploadService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    LoggerService loggerService;
+
+//    @Autowired
+//    UploadFlag uploadFlag;
 
     @Autowired
     UploadRepository uploadRepository;
@@ -31,9 +38,11 @@ public class UploadService {
         this.uploadUtil=uploadUtil;
     }
 
-    public void upload(MultipartFile file) throws Exception {
-
-
+    public void upload(MultipartFile file,
+                       String loggedUser,
+                       HttpServletResponse response) throws Exception {
+        final Integer[] greska = {new Integer(0)};
+        final Integer[] flag = {new Integer(0)};
         Path tempDir = Files.createTempDirectory("");
         File tempFile = tempDir.resolve(file.getOriginalFilename()).toFile();
         file.transferTo(tempFile);
@@ -54,18 +63,29 @@ public class UploadService {
                 String drzava_iso = row.getCell(3).getStringCellValue();
                 String sifra_valute = row.getCell(4).getStringCellValue();
                 String valuta = row.getCell(5).getStringCellValue();
-                System.out.println(valuta);
                 int jedinica = (int) row.getCell(6).getNumericCellValue();
                 String kupovni_tecaj = row.getCell(7).getStringCellValue();
                 String srednji_tecaj = row.getCell(8).getStringCellValue();
                 String prodajni_tecaj = row.getCell(9).getStringCellValue();
 
                 TecajeviUpload tecajeviUpload =new TecajeviUpload(broj_tecajnice, datum_primjene, drzava, drzava_iso, sifra_valute, valuta, jedinica, kupovni_tecaj, srednji_tecaj, prodajni_tecaj);
+                flag[0] =1;
                 uploadRepository.save(tecajeviUpload);
 
             }catch (Exception e){
+                greska[0]+=1;
+                if (greska[0]>1){
+                    System.out.println("greska");
+                    response.setStatus(400);
+                    logger.debug("Error - Uploadanje excela");
+                    loggerService.spremiLog("Error - Uploadanje excela", "/upload/", loggedUser, response.getStatus());
+                }
             }
         });
+        if (flag[0].equals(1)){
+            logger.debug("Uploadanje excela");
+            loggerService.spremiLog("Uploadanje excela", "/upload/", loggedUser, response.getStatus());
+        }
 
     }
 }
